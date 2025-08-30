765b53566de33d49673274e15df5b09cf5a23032
isaacmikhail <isaac_mikhail@hotmail.com>
2025-08-30 16:24:47 -0300

    Task3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
@@ -9,27 +10,35 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((acc,item)=>acc+item.cost*item.quantity, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+  onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
+
   };
 
   return (
@@ -62,7 +71,9 @@ const CartItem = ({ onContinueShopping }) => {
     </div>
   );
 };
+CartItem.propTypes = {
+  onContinueShopping: PropTypes.func.isRequired,
+};
 
 export default CartItem;
-
 
